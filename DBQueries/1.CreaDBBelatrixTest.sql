09351ca48f9a516868f5f834acc60680587af527
isaacsanchezastorayme <sanchezisaac.ti@hotmail.com>
2017-09-25 23:03:59 -0500

    Update to DB queries
    Update to DB queries

--- DBQueries/1.CreaDBBelatrixTest.sql
+++ DBQueries/1.CreaDBBelatrixTest.sql
@@ -1,8 +1,12 @@
+if not exists(select * from sys.databases where name = 'BelatrixTest')
 create database BelatrixTest
 go
 
 use BelatrixTest
 go
+
+IF  NOT EXISTS (SELECT * FROM sys.objects 
+WHERE object_id = OBJECT_ID(N'[dbo].[T_MODULO]') AND type in (N'U') )
 
 create table T_MODULO(
 MODULO_ID INT NOT NULL PRIMARY KEY,
@@ -15,6 +19,15 @@ GO
 
 INSERT INTO T_MODULO (MODULO_ID, MODULO_DESCRIPCION, MODULO_URL, MODULO_ESTADO, MODULO_OBSERVACION)
 VALUES (1, 'PORTAL', '', 1, 'Belatrix test uno')
+GO
+
+IF EXISTS (
+        SELECT type_desc, type
+        FROM sys.procedures WITH(NOLOCK)
+        WHERE NAME = 'uspObtenerModulos'
+            AND type = 'P'
+      )
+     DROP PROCEDURE dbo.uspObtenerModulos
 GO
 
 CREATE PROCEDURE [dbo].[uspObtenerModulos]    
--- DBQueries/2.CreaLoggerDBtables.sql
+++ DBQueries/2.CreaLoggerDBtables.sql
@@ -1,3 +1,6 @@
+IF  NOT EXISTS (SELECT * FROM sys.objects 
+WHERE object_id = OBJECT_ID(N'[dbo].[Logs]') AND type in (N'U') )
+
 CREATE TABLE [dbo].[Logs](
  [ID] [INT]	IDENTITY(1,1) NOT NULL,
  [Level] [varchar] (255) NOT NULL,
@@ -12,6 +15,14 @@ GO
 ALTER TABLE [dbo].[Logs] ADD CONSTRAINT [DF_Logs_TimeStamp] DEFAULT (getdate()) FOR [LoggedOnDate]
 GO
 
+IF EXISTS (
+        SELECT type_desc, type
+        FROM sys.procedures WITH(NOLOCK)
+        WHERE NAME = 'InsertLog'
+            AND type = 'P'
+      )
+     DROP PROCEDURE dbo.InsertLog
+GO
 
 CREATE PROCEDURE [dbo].[InsertLog]
 (
